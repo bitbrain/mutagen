9e1694edf3bcb615948c4b472bdbf88d5dd49c9b
bitbrain <miguel-gonzalez@gmx.de>
2026-06-13 18:37:18 +0100

    feat(input): press R to reset

--- scenes/gameplay_scene.gd
+++ scenes/gameplay_scene.gd
@@ -8,5 +8,10 @@ func _ready() -> void:
 	player.mutated.connect(_player_mutated)
 
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("reset"):
+		get_tree().change_scene_to_file(get_tree().current_scene.scene_file_path)
+
+
 func _player_mutated(mutagen_color:MutagenColor):
 	get_tree().call_group("door", "change_mutation", mutagen_color)
