b9884d5ba8c91cfe6fd9c05525f0a9407af875a8
bitbrain <miguel-gonzalez@gmx.de>
2026-06-18 20:31:27 +0100

    introduce gameplay card

--- scenes/game_complete_scene.gd
+++ scenes/game_complete_scene.gd
@@ -3,6 +3,8 @@ extends Node2D
 # FIXME: cannot use this! Godot seems to break completely here... :(
 #const STAGE_1 = preload("res://scenes/stages/stage_1.tscn")
 
+const TRANSITION_MS = 0.3
+const TRANSITION_DELAY = 0.3
 
 
 @onready var button: Button = %Button
@@ -14,3 +16,7 @@ func _ready() -> void:
 	PlayerStats.reset(true)
 	token_count.text = "You found " + str(tokens) + "token" + ("" if tokens == 1 else "s")
 	button.pressed.connect(func(): get_tree().change_scene_to_file("res://scenes/stages/stage_1.tscn"))
+
+	var transition_tween = create_tween()
+	transition_tween.tween_property(VFX, "transition_amount", 1.5, TRANSITION_DELAY)\
+	.set_delay(TRANSITION_DELAY)
