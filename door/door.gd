967f95f1f9e10a2c20764acd8f9d3b7ba7371f1a
bitbrain <miguel-gonzalez@gmx.de>
2026-06-21 07:50:10 +0100

    animate door dissolve/restore

--- door/door.gd
+++ door/door.gd
@@ -12,16 +12,26 @@ extends StaticBody2D
 
 
 var mutagen_color:MutagenColor = MutagenColor.DEFAULT
-
+var dissolved = false
 
 func _ready() -> void:
+	# ensure we always play the normal animation after animation finished
+	sprite_2d.animation_finished.connect(func():
+		if sprite_2d.animation == "restore":
+			sprite_2d.play("default")
+		)
 	_update_color()
 
 
 func change_mutation(other_color:MutagenColor) -> void:
 	var same_mutation = self.mutagen_color.is_same(other_color)
-	visible = not same_mutation
-	set_collision_layer_value(1, visible)
+	set_collision_layer_value(1, not same_mutation)
+	if not dissolved and same_mutation:
+		dissolved = true
+		sprite_2d.play("dissolve")
+	if dissolved and not same_mutation:
+		dissolved = false
+		sprite_2d.play("restore")
 	
 
 
