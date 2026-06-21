05f3d194d3e7bfec9240a190a0a2d5d85f80639b
bitbrain <miguel-gonzalez@gmx.de>
2026-06-21 08:55:38 +0100

    pause support

---
+++ pause_manager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+var paused = false
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		paused = not paused
+		get_tree().paused = not paused
+		if paused:
+			Toast.show_text("Paused")
+		else:
+			Toast.show_text("Unpaused")
+		
